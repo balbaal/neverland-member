471450f0b87f92da51a64a26725dd52f740ecc72
Balbaal <iqbal.mi20@gmail.com>
2020-09-08 20:38:49 +0800

    change: script run dev

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./assets/css/style.output.css";
 function App() {
   return (
     <div className="container mx-auto">
-      <h2 className="h3 text-teal-500">Member Page</h2>
+      <h2 className="h1 text-pink-500">Member Page</h2>
     </div>
   );
 }
